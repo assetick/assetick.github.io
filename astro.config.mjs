c6f6f2e583dd5c9d68775329397d6f13f50718d3
Abhijeet <adhamankar@gmail.com>
2024-09-26 11:37:48 +0530

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: ['@fontsource-variable/inter', '@fontsource-variable/outfit']
+    }
+  },
+  site: "https://astro-theme-one.vercel.app/",
+  integrations: [
+    tailwind(),
+    icon(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+});
